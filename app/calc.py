0c2e53d6cc6239149d4e8b77abdfaebdbf13101f
Jaime Perez <jaime.perez@dualcorp.net>
2024-06-13 17:43:09 -0500

    unificacion practica 02

--- app/calc.py
+++ app/calc.py
@@ -1,3 +1,10 @@
+import app
+import math
+
+class InvalidPermissions(Exception):
+    pass
+
+
 class Calculator:
     def add(self, x, y):
         self.check_types(x, y)
@@ -8,6 +15,9 @@ class Calculator:
         return x - y
 
     def multiply(self, x, y):
+        if not app.util.validate_permissions(f"{x} * {y}", "user1"):
+            raise InvalidPermissions('User has no permissions')
+
         self.check_types(x, y)
         return x * y
 
@@ -25,6 +35,24 @@ class Calculator:
     def check_types(self, x, y):
         if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
             raise TypeError("Parameters must be numbers")
+        
+
+    #  codigo agregado    
+
+    def raiz_cuadrada(self, x):
+        self.check_type(x)
+        
+        return math.sqrt(x)
+
+    def logaritmo_base_diez(self, x):
+        self.check_type(x)
+        if x <= 0:
+            raise ValueError("El logaritmo de números no positivos no está definido")
+        return math.log10(x)
+
+    def check_type(self, x):
+        if not isinstance(x, (int, float)):
+            raise TypeError("El parámetro debe ser un número")    
 
 
 if __name__ == "__main__":  # pragma: no cover
